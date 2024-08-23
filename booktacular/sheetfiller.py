f445e9375dde8e592357d4941921290f0cf5e300
poikilos <7557867+poikilos@users.noreply.github.com>
2024-08-22 20:05:36 -0400

    Add more metadata and comments.

--- booktacular/sheetfiller.py
+++ booktacular/sheetfiller.py
@@ -6,12 +6,13 @@ FIELD_META_KEYS = [
     "base_attribute_", "rules", "default",
     "recommended_max_characters", "max_characters",
     "lines", "lambda", "value_deletes", "value_properties",
-    "delete_if_not", "keep"]
+    "delete_if_not", "keep", "line_prefix", "tally_indent"],
 # TODO: implement globals (such as those used by delete_if_not, and others starting with _)
 # TODO: if keep, keep initial value in sheet unless set (or deleted by a condition)
+# TODO: if tally_indent is set, code can indent that much if ability has a tally (is not cantrip)
 RULES_META_KEYS = ["lambdas"]
 LAMBDA_OPERATORS = ["+"]
-LAMBDA_FUNCTIONS = ["min"]
+LAMBDA_FUNCTIONS = ["min", "if"]
 
 DEFAULTS = {
     "heightened_help_": """<tspan
